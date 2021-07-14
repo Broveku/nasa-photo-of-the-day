f58b49e40a21e9901ef9daf0297c2c08e45c8422
Broveku <gearhearjordan@gmail.com>
2021-07-14 14:26:10 -0700

    finished a working api pullerino

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from 'axios'
+import { API_KEY ,BASE_URL } from './index'
+import { Player } from 'video-react'
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    axios.get(`${BASE_URL}/planetary/apod?api_key=${API_KEY}&date=1996-09-13`)
+    .then(res => {
+      console.log(res.data)
+      setData(res.data)
+    }).catch(err =>{
+      console.log(err)
+    })
+  },[])
+  
+  
+  
   return (
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun <span role="img" aria-label='go!'>🚀</span>!
       </p>
+      <h1>{data.title}</h1>
+      <h2>{data.date}</h2>
+      <img src ={data.url} alt = 'Black Hole'></img>
+      
+      
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+export const BASE_URL = 'https://api.nasa.gov'
+export const API_KEY = 'K7V1z6TpBgTMcJh8ezoGfFuFhVtxUT3zxCAeMAqz'
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
